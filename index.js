c1b551575adac1610c62d8807a9a534efa90e4fa
kunal <you@example.com>
2023-05-14 09:30:06 -0500

    asdfasdfaa

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.post("/text", async (req, resp) => {
     {
       headers: {
         "Content-Type": "application/json",
-        Authorization: `Bearer ${"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"}`,
+        Authorization: `Bearer ${"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"}`,
       },
     }
   );
@@ -41,7 +41,7 @@ app.post("/image", async (req, resp) => {
     {
       headers: {
         "Content-Type": "application/json",
-        Authorization: `Bearer ${"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"}`,
+        Authorization: `Bearer ${"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"}`,
       },
     }
   );
